58022f4002f36673eef72862ad24cde5ed7ebd20
Maks Tamoian <maks.tamoian@gmail.com>
2024-01-05 16:10:31 +0100

    day14 both parts solved

--- day14/main.rs
+++ day14/main.rs
@@ -1,5 +1,65 @@
 use std::{io::prelude::*, time::Instant};
 
+fn tilt_up(matrix: &mut Vec<Vec<char>>) {
+    for col in 0..matrix[0].len() {
+        for row in 0..matrix.len() {
+            if matrix[row][col] == 'O' {
+                let mut shift_row = row;
+                while shift_row > 0 && matrix[shift_row - 1][col] == '.' {
+                    matrix[shift_row][col] = '.';
+                    matrix[shift_row - 1][col] = 'O';
+                    shift_row -= 1;
+                }
+            }
+        }
+    }
+}
+
+fn tilt_down(matrix: &mut Vec<Vec<char>>) {
+    for col in 0..matrix[0].len() {
+        for row in (0..matrix.len()).rev() {
+            if matrix[row][col] == 'O' {
+                let mut shift_row = row;
+                while shift_row < matrix.len() - 1 && matrix[shift_row + 1][col] == '.' {
+                    matrix[shift_row][col] = '.';
+                    matrix[shift_row + 1][col] = 'O';
+                    shift_row += 1;
+                }
+            }
+        }
+    }
+}
+
+fn tilt_right(matrix: &mut Vec<Vec<char>>) {
+    for row in 0..matrix.len() {
+        for col in (0..matrix[0].len()).rev() {
+            if matrix[row][col] == 'O' {
+                let mut shift_col: usize = col;
+                while shift_col < matrix[0].len() - 1 && matrix[row][shift_col + 1] == '.' {
+                    matrix[row][shift_col] = '.';
+                    matrix[row][shift_col + 1] = 'O';
+                    shift_col += 1;
+                }
+            }
+        }
+    }
+}
+
+fn tilt_left(matrix: &mut Vec<Vec<char>>) {
+    for row in 0..matrix.len() {
+        for col in 0..matrix[0].len() {
+            if matrix[row][col] == 'O' {
+                let mut shift_col: usize = col;
+                while shift_col > 0 && matrix[row][shift_col - 1] == '.' {
+                    matrix[row][shift_col] = '.';
+                    matrix[row][shift_col - 1] = 'O';
+                    shift_col -= 1;
+                }
+            }
+        }
+    }
+}
+
 fn part_one() {
     let input = aoclib::file_reader::get_input("day14".to_string());
     let start_time = Instant::now();
@@ -12,22 +72,7 @@ fn part_one() {
         matrix.push(line.chars().collect());
     }
 
-    fn max_out_current_column(col: usize, matrix: &mut Vec<Vec<char>>) {
-        for row in 0..matrix.len() {
-            if matrix[row][col] == 'O' {
-                let mut shift_row = row;
-                while shift_row > 0 && matrix[shift_row - 1][col] == '.' {
-                    matrix[shift_row][col] = '.';
-                    matrix[shift_row - 1][col] = 'O';
-                    shift_row -= 1;
-                }
-            }
-        }
-    }
-
-    for col in 0..matrix[0].len() {
-        max_out_current_column(col, &mut matrix);
-    }
+    tilt_up(&mut matrix);
 
     for (row, line) in matrix.iter().enumerate() {
         let load = matrix.len() - row;
@@ -35,12 +80,11 @@ fn part_one() {
     }
 
     print!(
-        "Finished part 1 in: \x1b[1m{:#?}\x1b[0m with answer: \x1b[1m{:#?}\x1b[0m",
-        start_time.elapsed(), sum
+        "Finished part 1 in: \x1b[1m{:#?}\x1b[0m with answer: \x1b[1m{:#?}\x1b[0m\n",
+        start_time.elapsed(),
+        sum
     );
-
 }
-
 
 fn part_two() {
     let input = aoclib::file_reader::get_input("day14".to_string());
@@ -54,34 +98,11 @@ fn part_two() {
         matrix.push(line.chars().collect());
     }
 
-    fn tilt_up(col: usize, matrix: &mut Vec<Vec<char>>) {
-        for row in 0..matrix.len() {
-            if matrix[row][col] == 'O' {
-                let mut shift_row = row;
-                while shift_row > 0 && matrix[shift_row - 1][col] == '.' {
-                    matrix[shift_row][col] = '.';
-                    matrix[shift_row - 1][col] = 'O';
-                    shift_row -= 1;
-                }
-            }
-        }
-    }
-
-    fn tilt_down(col: usize, matrix: &mut Vec<Vec<char>>) {
-        for row in 0..matrix.len() {
-            if matrix[row][col] == 'O' {
-                let mut shift_row = row;
-                while shift_row > 0 && matrix[shift_row - 1][col] == '.' {
-                    matrix[shift_row][col] = '.';
-                    matrix[shift_row - 1][col] = 'O';
-                    shift_row -= 1;
-                }
-            }
-        }
-    }
-
-    for col in 0..matrix[0].len() {
-        tilt_up(col, &mut matrix);
+    for _ in 0..1000 {
+        tilt_up(&mut matrix);
+        tilt_left(&mut matrix);
+        tilt_down(&mut matrix);
+        tilt_right(&mut matrix);
     }
 
     for (row, line) in matrix.iter().enumerate() {
@@ -90,12 +111,13 @@ fn part_two() {
     }
 
     print!(
-        "Finished part 2 in: \x1b[1m{:#?}\x1b[0m with answer: \x1b[1m{:#?}\x1b[0m",
-        start_time.elapsed(), sum
+        "Finished part 2 in: \x1b[1m{:#?}\x1b[0m with answer: \x1b[1m{:#?}\x1b[0m\n",
+        start_time.elapsed(),
+        sum
     );
-
 }
 
 fn main() {
     part_one();
+    part_two();
 }
